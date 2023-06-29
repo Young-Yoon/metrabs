362ff41c70d41766d766d76295f34d370716631f
smoon <smoon@roblox.com>
2023-06-29 07:37:25 +0000

    evaluation bash update

--- src/evaluation.sh
+++ src/evaluation.sh
@@ -4,7 +4,7 @@ model_root="/home/jovyan/runs/metrabs-exp/"
 
 
 declare -a arr=(
-    "partial_ms_s1/112in_j8"
+    "partial_ms_s1/128in_j8"
 #    "/path/to/checkpoint/dir2"
 #    "/path/to/checkpoint/dir3"
     # Add more directories as needed
@@ -12,5 +12,8 @@ declare -a arr=(
 
 # Run the command for each directory
 for dir in "${arr[@]}"; do
-    ./main.py --predict --dataset=h36m --checkpoint-dir="${model_root}/${dir}" --backbone=mobilenetV3Small --init=scratch --mobilenet-alpha=1.0 --model-class=Metro --final-transposed-conv=1 --stride-test=32 --upper-bbox --upper-bbox-ratio 0.5 0.5 --proc-side=160 --output-upper-joints
+    ./main.py --predict --dataset=h36m --checkpoint-dir="${model_root}/${dir}" --backbone=mobilenetV3Smalls1 --init=scratch --mobilenet-alpha=1.0 --model-class=Metro --final-transposed-conv=1 --stride-test=32 --upper-bbox --upper-bbox-ratio 0.5 0.5 --proc-side=128 --output-upper-joints
+    python -m eval_scripts.eval_h36m --pred-path="${model_root}/${dir}"/predictions_h36m.npz --root-last
+    ./main.py --predict --dataset=h36m --checkpoint-dir="${model_root}/${dir}" --backbone=mobilenetV3Smalls1 --init=scratch --mobilenet-alpha=1.0 --model-class=Metro --final-transposed-conv=1 --stride-test=32 --proc-side=128 --output-upper-joints
+    python -m eval_scripts.eval_h36m --pred-path="${model_root}/${dir}"/predictions_h36m.npz --root-last    
 done
