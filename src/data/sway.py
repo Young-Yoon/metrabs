94b63bcb1aeffc6b88e2540c679cd1520b00c6e8
ylee <ylee@roblox.com>
2023-07-14 22:18:26 +0000

    data loading for SWAY dataset annotated by Metrabs

--- src/data/sway.py
+++ src/data/sway.py
@@ -35,11 +35,112 @@ def vis(imagepath, projected_2d, bbox, keypoint_2d=None):
     return
 
 
+def get_seq_info(phase, root_sway, use_kd):
+    if use_kd:
+        frame_step = 5 if phase in {'train'} else 60
+        with open(f'{root_sway}/dataset61769.txt', "r") as f:
+            seq_names = [line.strip() for line in f.readlines()]
+        parts = [12000//100*p for p in (0, 90, 95, 100)]
+        pid = {'train':0, 'validation':1, 'test':2}
+        seq_names = seq_names[parts[pid[phase]]:parts[pid[phase]+1]]
+        seq_folders = ['sway61769']
+    else:
+        frame_step = 30 if phase in {'train'} else 64
+        with open(f'{root_sway}/{phase}.txt', "r") as f:
+            seq_names = [line.strip() for line in f.readlines()]
+        if phase in {'train'}:
+            seq_names = seq_names[70:]
+        if phase in {'test'}:
+            seq_folders = ['sway61769'] + ['sway_test_variants/'+v for v in ['landscape', 'portrait', 'tight']]
+            print(seq_folders)
+        else:
+            seq_folders = ['sway61769']
+    return seq_names, seq_folders, frame_step
 
-@util.cache_result_on_disk(f'{paths.CACHE_DIR}/sway4test.pkl', min_time="2023-06-27T11:30:43")
-def make_sway():
+
+def load_seq_param(seq_dir, seq_name, root_sway, use_kd):
+    seq_path = os.path.join(root_sway, seq_dir, seq_name)
+    if use_kd:
+        fi, fe, fw, fb = (os.path.join(seq_path, "metrab_annotations", f) for f in ["intrinsics.npy", "extrinsics.npy", "poses3d.npy", "bbox.npy"])
+        if not os.path.exists(fi):
+            return False, None
+    else:
+        fi, fe, fw, fb = (os.path.join(seq_path, f) for f in ["intrinsics.npy", "extrinsics.npy", "wspace_poses3d.npy", "bbox.npy"])
+
+    intrinsics, extrinsics = np.load(fi), np.load(fe)
+    if np.isnan(extrinsics).any() or np.isnan(intrinsics).any():
+        return False, None
+    if len(intrinsics.shape) == 2:
+        camera = cameralib.Camera(
+            extrinsic_matrix=extrinsics, intrinsic_matrix=intrinsics,
+            world_up=(0, 1, 0))
+    else:
+        camera = None
+
+    if use_kd:
+        world_pose3d = np.load(fw, allow_pickle=True).item()
+        bbox = np.load(fb, allow_pickle=True).item()
+        n_frames = int(list(world_pose3d.keys())[-1])
+    else:
+        world_pose3d = np.load(fw)
+        bbox = np.load(fb)
+        if np.isnan(world_pose3d).any() or np.isnan(bbox).any():
+            return False, None
+        n_frames = world_pose3d.shape[0]
+    return True, (camera, world_pose3d, bbox, n_frames)
+    
+    
+    
+def get_examples(phase, pool, use_kd=True):
+    result = []
     root_sway = f'{paths.DATA_ROOT}/sway'
+    seq_names, seq_folders, frame_step = get_seq_info(phase, root_sway, use_kd)
 
+    for seq_dir, seq_name in util.progressbar(itertools.product(seq_folders, seq_names)):
+        load_success, params = load_seq_param(seq_dir, seq_name, root_sway, use_kd)
+        if not load_success:
+            continue
+        camera, world_pose3d, bbox, n_frames = params
+
+        prev_coords = None
+
+        for i_frame in range(0, n_frames, frame_step):
+            if camera is None:  # len(intrinsics.shape) == 3:
+                camera = cameralib.Camera(
+                    extrinsic_matrix=extrinsics, intrinsic_matrix=intrinsics[i_frame],
+                    world_up=(0, 1, 0))
+            if use_kd:
+                fr_idx = f'{i_frame+1:05d}'
+                if fr_idx not in world_pose3d.keys():
+                    continue
+                world_coords = world_pose3d[fr_idx]
+                bbox_fr = bbox[fr_idx]
+            else:
+                world_coords = world_pose3d[i_frame]
+                world_coords = world_coords[i_relevant_joints, :]
+                bbox_fr = bbox[i_frame]
+
+            if (phase == 'train' and prev_coords is not None and
+                    np.all(np.linalg.norm(world_coords - prev_coords, axis=1) < 100)):
+                continue
+            prev_coords = world_coords
+
+            impath = f'sway/{seq_dir}/{seq_name}/images/{i_frame+1:05d}.jpg'
+            ex = ps3d.Pose3DExample(impath, world_coords, bbox=bbox_fr, camera=camera)
+
+#                 vis(os.path.join(paths.DATA_ROOT, impath), proj2d, bbox[i_frame])
+#                 new_image_relpath = impath.replace('sway/sway61769', 'sway_downscaled')
+#                 pool.apply_async(
+#                     make_efficient_example,
+#                     (ex, new_image_relpath),
+#                     callback=result.append)
+            result.append(ex)
+
+    return result
+
+#'sway4test.pkl': include sway_test_variants
+@util.cache_result_on_disk(f'{paths.CACHE_DIR}/sway_kd12k.pkl', min_time="2023-06-27T11:30:43")
+def make_sway():
     joint_names = (
         'rhip,rkne,rank,lhip,lkne,lank,tors,neck,head,htop,'
         'lsho,lelb,lwri,rsho,relb,rwri,pelv'.split(','))
@@ -56,62 +157,6 @@ def make_sway():
     
     joint_info = ps3d.JointInfo(joint_names, edges)
     i_relevant_joints = [2, 5, 8, 1, 4, 7, 9, 12, 15, 15, 16, 18, 20, 17, 19, 21, 0]
-
-    def get_examples(phase, pool):
-        frame_step = 30 if phase in {'train'} else 64
-        result = []
-        with open(f'{root_sway}/{phase}.txt', "r") as f:
-            seq_names = [line.strip() for line in f.readlines()]
-        if phase in {'train'}:
-            seq_names = seq_names[70:]
-        if phase in {'test'}:
-            seq_folders = ['sway61769'] + ['sway_test_variants/'+v for v in ['landscape', 'portrait', 'tight']]
-            print(seq_folders)
-        else:
-            seq_folders = ['sway61769']
-
-        for seq_dir, seq_name in util.progressbar(itertools.product(seq_folders, seq_names)):
-            seq_path = os.path.join(root_sway, seq_dir, seq_name)
-            intrinsics = np.load(os.path.join(seq_path, "intrinsics.npy"))
-            extrinsics = np.load(os.path.join(seq_path, "extrinsics.npy"))
-            if np.isnan(extrinsics).any() or np.isnan(intrinsics).any():
-                continue
-            if len(intrinsics.shape) == 2:
-                camera = cameralib.Camera(
-                    extrinsic_matrix=extrinsics, intrinsic_matrix=intrinsics,
-                    world_up=(0, 1, 0))
-
-            world_pose3d = np.load(os.path.join(seq_path, "wspace_poses3d.npy"))
-            bbox = np.load(os.path.join(seq_path, "bbox.npy"))
-            if np.isnan(world_pose3d).any() or np.isnan(bbox).any():
-                continue
-            n_frames = world_pose3d.shape[0]
-            prev_coords = None
-            
-            for i_frame in range(0, n_frames, frame_step):
-                if len(intrinsics.shape) == 3:
-                    camera = cameralib.Camera(
-                        extrinsic_matrix=extrinsics, intrinsic_matrix=intrinsics[i_frame],
-                        world_up=(0, 1, 0))
-                world_coords = world_pose3d[i_frame]
-                world_coords = world_coords[i_relevant_joints, :]
-                if (phase == 'train' and prev_coords is not None and
-                        np.all(np.linalg.norm(world_coords - prev_coords, axis=1) < 100)):
-                    continue
-                prev_coords = world_coords
-
-                impath = f'sway/{seq_dir}/{seq_name}/images/{i_frame+1:05d}.jpg'
-                ex = ps3d.Pose3DExample(impath, world_coords, bbox=bbox[i_frame], camera=camera)
-
-#                 vis(os.path.join(paths.DATA_ROOT, impath), proj2d, bbox[i_frame])
-#                 new_image_relpath = impath.replace('sway/sway61769', 'sway_downscaled')
-#                 pool.apply_async(
-#                     make_efficient_example,
-#                     (ex, new_image_relpath),
-#                     callback=result.append)
-                result.append(ex)
-                
-        return result
             
     with util.BoundedPool(None, 120) as pool:
         train_examples = get_examples('train', pool)
--- src/eval_scripts/eval_sway.py
+++ src/eval_scripts/eval_sway.py
@@ -10,7 +10,7 @@ import re
 import numpy as np
 import spacepy
 
-import data.h36m
+import data.sway
 import options
 import paths
 import util
@@ -44,7 +44,7 @@ def evaluate(pred_path, all_true3d, test_var):
     all_pred3d -= all_pred3d[:, i_root, np.newaxis]
     all_true3d -= all_true3d[:, i_root, np.newaxis]
     
-    test_variants = ('', 'landscape', 'portrait', 'tight')
+    test_variants = ('')  # ('', 'landscape', 'portrait', 'tight')
     n_joints_up = 8
     n_joints_pred = all_pred3d.shape[1]
     if FLAGS.procrustes:
@@ -61,41 +61,40 @@ def evaluate(pred_path, all_true3d, test_var):
     metrics += [np.mean(dist[test_var == tv]) for tv in test_variants]
     return metrics
 
+
 def to_latex(numbers):
     return ', '.join([f'{x:.1f}' for x in numbers])
 
 
-def get_all_gt_poses():
-    root_sway = f'{paths.DATA_ROOT}/sway'
-    frame_step = 64
+def get_all_gt_poses(use_kd=True):
     all_world_coords = []
     all_image_relpaths = []
+    root_sway = f'{paths.DATA_ROOT}/sway'
+    seq_names, seq_folders, frame_step = data.sway.get_seq_info('test', root_sway, use_kd)
 
-    i_relevant_joints = [2, 5, 8, 1, 4, 7, 9, 12, 15, 15, 16, 18, 20, 17, 19, 21, 0]
-    if not FLAGS.root_last:
-        i_relevant_joints = [i_relevant_joints[-1]] + i_relevant_joints[:-1]
-
-    seq_folders = ['sway61769'] + ['sway_test_variants/'+v for v in ['landscape', 'portrait', 'tight']]
-    with open(f'{root_sway}/test.txt', "r") as f:
-        seq_names = [line.strip() for line in f.readlines()]
+    if not use_kd:
+        i_relevant_joints = [2, 5, 8, 1, 4, 7, 9, 12, 15, 15, 16, 18, 20, 17, 19, 21, 0]
+        if not FLAGS.root_last:
+            i_relevant_joints = [i_relevant_joints[-1]] + i_relevant_joints[:-1]
 
     for seq_dir, seq_name in util.progressbar(itertools.product(seq_folders, seq_names)):
-        seq_path = os.path.join(root_sway, seq_dir, seq_name)
-        world_pose3d = np.load(os.path.join(seq_path, "wspace_poses3d.npy"))
-        if np.isnan(world_pose3d).any():
+        load_success, params = data.sway.load_seq_param(seq_dir, seq_name, root_sway, use_kd)
+        if not load_success:
             continue
-        bbox = np.load(os.path.join(seq_path, "bbox.npy"))
-        if np.isnan(bbox).any():
-            continue
+        _, world_pose3d, _, n_frames = params
 
-        n_frames_total = len(world_pose3d)
-        world_coords = world_pose3d[::frame_step]
-        world_coords = world_coords[:, i_relevant_joints, :].astype(np.single)
-        all_world_coords.append(world_coords)
         image_relfolder = f'sway/{seq_dir}/{seq_name}/images'
-        all_image_relpaths += [
-            f'{image_relfolder}/{i_frame+1:05d}.jpg'
-            for i_frame in range(0, n_frames_total, frame_step)]
+        if use_kd:
+            fr_idx = [f'{i_frame+1:05d}' for i_frame in range(0, n_frames, frame_step) if f'{i_frame+1:05d}' in world_pose3d.keys()]
+            world_coords = np.array([world_pose3d[fr] for fr in fr_idx], dtype=np.single)
+            all_image_relpaths += [f'{image_relfolder}/{fr}.jpg' for fr in fr_idx]
+        else:
+            world_coords = world_pose3d[::frame_step]
+            world_coords = world_coords[:, i_relevant_joints, :].astype(np.single)
+            all_image_relpaths += [
+                f'{image_relfolder}/{i_frame+1:05d}.jpg'
+                for i_frame in range(0, n_frames, frame_step)]
+        all_world_coords.append(world_coords)
 
     order = np.argsort(all_image_relpaths)
     all_world_coords = np.concatenate(all_world_coords, axis=0)[order]
